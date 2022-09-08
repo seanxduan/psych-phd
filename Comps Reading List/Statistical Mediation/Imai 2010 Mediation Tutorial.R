804b275d42d561f129cd567be41c133f20c22e24
Sean Duan <seanxduan@gmail.com>
2022-09-08 15:51:59 -0500

    Braining hard on the Imai tutorial from advances in SSR using R

---
+++ Comps Reading List/Statistical Mediation/Imai 2010 Mediation Tutorial.R
@@ -0,0 +1,167 @@
+# From "Advances in Social Science Research Using R #
+### Chapter 8: Causal Mediation Analysis Using R ####
+
+#following up from Imai 2008 and Imai 2009 algorithims to estimate causal mediation
+#we can estimate causal mediation effects for linear and nonlinear relations
+
+#How to do this in R?
+
+install.packages("mediation")
+library("mediation")
+update.packages("mediation")
+#mediation is up to date
+?mediation
+
+#this is the design and architecture overview of the Mediation package
+# details of the methods can be found within Imai 2010a and 2010b
+
+#mediation relies on sequential ignorability assumption in previous paper
+#Sequential ignorability states:
+#The observed mediator status is as if randomly assigned condition
+#on the randomized treatment variable and the pretreatment covariates
+
+#requires 2 statistical models, one for mediator: f (Mi | Ti,Xi)
+#Other for outcome: f (Yi | Ti,Mi,Xi)
+
+#once these models are chosen and fitted, then mediation will compute the 
+#estimated causal mediation effect, and other estimates using the Imai 2010 algos.
+
+#The algos produce CI's based on either nonparametric bootstrap (for para and nonpara models)
+#or quasi-Bayesian Monte Carlo approximation (for parametric only)
+
+#Three steps for mediation analysis
+# 1: Fit mediator and outcome models using for example, regression models
+# e.g. the usual lm() and glm() functions
+
+# 2: Analyst takes the output objects from these models (e.g. model.m and model.y)
+# We then use these output objects as inputs for our main function, mediate()
+
+# 3: Mediate function then estimates the causal mediation effects, direct effects
+# and total effect, along with uncertainty estimates.
+
+# 4: FINALLY, sensitivity analysis can be done using medsens() function,
+# this function takes the output of mediate as an input
+
+# Generally, for the output of the 'mediate' function, a summary() method reports
+# the key findings in table form, medsens() can also output as summary() or plot()
+
+
+################################################
+#### Estimation of Causal Mediation Effects ####
+################################################
+
+#Estimation of these effects is based off of algorithms 1 and 2 of Imai 2010
+# How are these algorithms implemented in mediation()?
+
+#######################################
+## Algorithm 1 for Parametric Models ##
+#######################################
+
+#Start by fitting parametric models for mediator and out come variables
+#e.g. we model observed mediatior Mi given treatment Ti and pretreatment 
+#covariates Xi. 
+
+#Similarly, model observed outcome Yi given the treatment, mediator, and
+# pretreatment covariates.
+
+#For example, to implement the original Baron-Kenny procedure, linear models
+# are fitted for both the mediator and outcome models using the lm command
+
+#The model outputs from these two parametric models form the inputs for the
+#mediate() function, the user supplies the names for which variables are the
+#mediator and the ou tcome, as well as what # of simulations should be used
+#for inference, and whether the mediator variable is allowed to interact with the
+#treatment variable in the outcome model (no-interaction assumption is the norm,
+# but it's not necessarily realistic)
+
+#given these model objects, the estimation proceeds by simulating the model
+#parameters based on their approximate asymptotic distribution (the multivariate
+# normal distribution w/ mean = to parameter estimates, and variance = to 
+# asymptotic variance estimate)
+
+#we then compute causal mediation effects of interest for each parameter draw
+#this method of inference is similar to an approximation of the Bayesian
+#posterior distribution due to Bernstein-von Mises Theorem (??)
+#this is VERY computationally efficient compared to algo #2
+
+#R's object oriented works well in mediate(), functions like coef() and vcov()
+#are used to extract the point and uncertainty estimates from the model objects
+#to form the multivariate normal distribution from which parameter draws
+#are sampled. Additionally, computation of estimated causal mediation effects
+#requires prediction of mediator values under different treatment regimes as well
+#as the prediction of outcome values under different treatment and mediator values
+
+#We can do this using 'model.frame()' to set treatment and mediator values to
+#specific levels, while keeping values of OTHER variables unchanged! 
+#Model.matrix and matrix multiplication is then used with the distribution of 
+#simulated parameters to compute the mediation and direct effects
+
+#the biggest strength: APPLICABLE TO A WIDE RANGE OF PARAMETRIC MODELS
+# allows us to avoid coding a completely seperate function for different models!
+
+##################################################
+## Algorithm 2 for Non/Semiparametric Inference ##
+##################################################
+
+#main disadvantage of algo 1 is that it isn't easily applied to non/semiparametric
+#models. For these models, algo 2, based on nonparametric bootstrp, can be used
+# HOWEVER, it is MUCH more computationally intensive.
+
+#Algo 2 can also be used for our parametric models; Algorithm 2 resamples the
+#observed data with replacement. Then for each of the bootstrapped samples, we fit
+#both the outcome and the mediator models, and compute the quantities of interest
+#As in algo 1, this requires the prediction of mediator values under different
+#treatment regimes, as well as the prediction of outcome values under different
+#treatment and mediator values. 
+
+# R's leverages object orientation here for value, Algo 2 relies on predict()
+#function to compute these predictions, while the algo manipulates the treatment
+#and mediator status using the model.frame function. This process is repeated
+#a large # of times and returns a bootstrap distribution of the mediation
+#direct, and total effects. Algorithm 2 allows analysts to estimate mediation
+#effects with more flexible model specifications, or to estimate mediation
+#effects for quantiles of the distribution
+
+##########################
+## Sensitivity Analysis ##
+##########################
+
+#Causal mediation analysis relies on sequential ignorability assumption
+#that BY DEFINITION cannot be verified with the observed data, this assumption
+#implies that the treatment is ignorable given the observed pretreatment
+#confounders, and that the mediator is ignorable given the observed treatment
+#and observed pretreatment covariates.
+
+#in order to probe the plausibility of this key identification assumption, we need
+#to perform sensitivity analysis. Which historically has been hard af to do for
+#a general form that applies to any parametric or nonparametric model.
+#Imai 2010 balled out and developed this analysis to work for commonly used models
+
+
+##Baron-Kenny Procedure##
+
+#Imai develops a sensitivity analysis based on the Baron-Kenny procedure
+#which is generalised to the LSEM with an interaction term.
+#the sensitivity parameter, Rho, is equal to the correlation between
+#the two error terms, for the prediction of mediator and outcome.
+
+#Under sequential ignorability, Rho is equal to zero, and thus the magnitude
+#of this correlation coefficient represents the departure from the ignorability
+#assumption (about the mediator)
+
+#Note that our treatment is assumed to be ignorable, as that's the case in 
+#randomized experiments with randomized treatments, but the mediator is not.
+#Our theorem shows how the ACME changes as a function of Rho
+
+#To obtain the CI's for sensitivity anlaysis, an iterative algo is applied
+# for a fixed value of rho, at the t'th iteration, given the current values and 
+#a given error correlation value (Rho), we compute the var/cov matrix of 
+#our two errors, and then we update the parameters via generalized least squares
+
+#this is essentially an application of the iterative feasible generalized
+#least square algorithm.
+
+
+
+
+
