b680eb58fe44837c4201e187c309df39714bf84f
Sean Duan <seanxduan@gmail.com>
2023-07-27 16:50:54 -0500

    pogged out and got our data cleaning setup - we can try for some preliminary analysis later :)

--- Study 1 data and analysis/Pilot analysis.R
+++ Study 1 data and analysis/Pilot analysis.R
@@ -31,7 +31,7 @@ v<-c(1/19, 1/7, 1/4, 3/8, 1/2,
      1/19, 4/7, 7/5, 13/9, 8/3,
      11/4, 10/3, 7/2, 17/4, 9/2)
 length(v)
-s_value<-matrix(v, nrow=length(pilot_data), ncol=length(v), byrow=TRUE)
+s_value<-matrix(v, nrow=nrow(pilot_data), ncol=length(v), byrow=TRUE)
 
 nl_ans<-number_line - s_value
 nl_ans[,11:20]<-(nl_ans[,11:20]/5)
@@ -64,6 +64,59 @@ pilot_data_w_score<-pilot_data[,-c(65:92)]
 ?write.csv
 write.csv(pilot_data_w_score, file = "pilot_data_score.csv")
 
+pilot_data_w_score<-read.csv("pilot_data_score.csv")
+
 #figure out how to structure the names themselves to be more functional?
 #maybe just do it in xl :eyes: lol
-names(pilot_data_w_score)[]
+
+which( colnames(pilot_data_w_score)=="Q121_4")
+
+#no we're just gonna slug through in R b/c we can
+
+#for the final responses
+
+which( colnames(pilot_data_w_score)=="Q121_4")
+names(pilot_data_w_score)[31:34] <- c("Post_High_1_10", "Post_High_2_10", "Post_High_3_10","Post_High_4_10")
+
+which( colnames(pilot_data_w_score)=="Q176_1")
+names(pilot_data_w_score)[54:57] <- c("Post_Low_1_10", "Post_Low_2_10", "Post_Low_3_10","Post_Low_4_10")
+
+
+#for the guesses
+which( colnames(pilot_data_w_score)=="Post_Int_1_10")
+names(pilot_data_w_score)[19:22] <- c("Pre_High_Guess_1", "Pre_High_Guess_2", "Pre_High_Guess_3","Pre_High_Guess_4")
+
+which(colnames(pilot_data_w_score)=="Q163_10")
+names(pilot_data_w_score)[42:45] <- c("Pre_Low_Guess_1", "Pre_Low_Guess_2", "Pre_Low_Guess_3","Pre_Low_Guess_4")
+
+#and for the surprise score?
+which( colnames(pilot_data_w_score)=="Q114_4")
+names(pilot_data_w_score)[23:26] <- c("High_Sup_1", "High_Sup_2", "High_Sup_3","High_Sup_4")
+
+which( colnames(pilot_data_w_score)=="Q169_4")
+names(pilot_data_w_score)[46:49] <- c("Low_Sup_1", "Low_Sup_2", "Low_Sup_3","Low_Sup_4")
+
+#nope gotta set age, gender ,etc. damn
+
+
+#names are set, lets try to get the 'melt' done properly?
+
+pilot_data_long<-melt(setDT(pilot_data_w_score),measure=patterns("1_10", "2_10", "3_10", "4_10",
+                                                                 "Sup_1", "Sup_2", "Sup_3", "Sup_4",
+                                                                 "Age","Gender","Race","School", "Like_Free_R","Hard_Free_R"), 
+               value.name=c("P1", "P2", "P3", "P4",
+                            "Suprise_1","Suprise_2","Suprise_3", "Suprise_4",
+                            "Age","Gender","Race","School_Year","Like_Free_Response", "Hard_Free_Response"),
+               variable.name="condition")
+
+#try to clean our our empty rows then check if it looks g
+ind<-which(is.na(pilot_data_long$P1))
+pilot_long_clean<-pilot_data_long[!ind,]
+
+ind<-which(is.na(pilot_long_clean$P2))
+pilot_long_clean<-pilot_long_clean[!ind,]
+
+#nice mostly good HOWEVER we should probably reset and get our 'guesses' set up as well RIP
+#or we can cut them out entirely? regardless, GJ
+
+write.csv(pilot_long_clean,'pilot_cleanish.csv')
